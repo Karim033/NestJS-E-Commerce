d4f0fec31c57b614883d3eb10c3e88bc0d8c3048
Karim033 <gaberk033@gmail.com>
2025-01-09 03:14:31 +0200

    add userSchema for app

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -5,8 +5,57 @@ export type UserDocument = HydratedDocument<User>;
 
 @Schema({ timestamps: true })
 export class User {
-  @Prop()
+  @Prop({
+    type: String,
+    required: [true, 'Name is required'],
+    min: [3, 'Name must be at least 3 characters'],
+    max: [30, 'Name must be at most 30 characters'],
+  })
   name: string;
+
+  @Prop({
+    type: String,
+    required: [true, 'Email is required'],
+    unique: [true, 'Email must be unique'],
+  })
+  email: string;
+  @Prop({
+    type: String,
+    required: [true, 'Password is required'],
+    min: [3, 'Password must be at least 3 characters'],
+    max: [20, 'Password must be at most 20 characters'],
+  })
+  password: string;
+
+  @Prop({
+    type: String,
+    required: [true, 'Role is required'],
+    enum: ['Admin', 'Manger', 'User'],
+  })
+  role: string;
+  @Prop({ type: String, enum: ['male', 'female'] })
+  gender: string;
+
+  @Prop({ type: Boolean, enum: [true, false] })
+  active: boolean;
+
+  @Prop({
+    type: String,
+  })
+  avatar: string;
+  @Prop({
+    type: Number,
+  })
+  age: number;
+
+  @Prop({ type: String })
+  phoneNumber: string;
+
+  @Prop({ type: String })
+  address: string;
+
+  @Prop({ type: String })
+  verficationCode: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class UserService {
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<User>,
+  ) {}
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    return this.userModel.create(createUserDto);
   }
 
   findAll() {
-    return `This action returns all user`;
+    return this.userModel.find();
   }
 
   findOne(id: number) {
